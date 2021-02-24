ec47d817d96493bc1fd3d92659ba25605f2fec96
luismasuelli <luis@triplemint.com>
2021-02-24 13:42:37 -0500

    Initial commit.

---
+++ action.go
@@ -0,0 +1,15 @@
+package make_tree
+
+import "io"
+
+// Actions are the callbacks to use to
+// make a particular filesystem tree.
+type Action interface {
+	// Executes the direct action. It must
+	// return an error on failure.
+	Do(currentDirectory string, dump io.Writer) error
+	// Executes the inverse action. It should
+	// not return error on failure, but silently
+	// forgive.
+	Rollback(currentDirectory string, dump io.Writer)
+}
